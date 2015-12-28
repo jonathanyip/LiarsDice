73b2aec039109e22dab2ce55d43d756893bee187
Jonathan Yip <jonathangyip@gmail.com>
2015-12-27 18:43:43 -0800

    Added socket.io

--- liarsdice.js
+++ liarsdice.js
@@ -12,7 +12,7 @@ app.set('view cache', false);
 /* Local static files */
 app.use(express.static('static'));
 
-/* Configure all views */
+/* Initialize all views */
 var views = require(__dirname + '/views.js')(app);
 
 /* Start server with the process defined port (or 3000) */
@@ -21,5 +21,8 @@ var server = app.listen(port, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	
-	console.log("Server is currently working.");
+	console.log("Server is currently working!");
 });
+
+/* Initialize socket */
+var socket = require(__dirname + '/socket.js')(server);
